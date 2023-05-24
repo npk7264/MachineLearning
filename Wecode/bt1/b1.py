546c661972ae750c0305286572911e7034ba0db0
Khang Nguyen Phuc <npkhang721@gmail.com>
2023-05-24 20:15:12 +0700

    create 10 folders

--- Wecode/bt1/b1.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-s = 0
-for i in range(1, n):
-    if n % i == 0:
-        s = s + i
-print(s)
--- Wecode/bt1/b4.py
+++
@@ -1,3 +0,0 @@
-PSI = float(input())
-
-print('{:g}'.format(PSI * 0.453592 / pow(2.54, 2), 6))
